0b7a134bd5336af67c93a4e54dedcc7860e9240a
Kacper Zacharz <xxygrek@wp.pl>
2022-05-05 15:04:55 +0200

    connection test

--- src/components/authentication/login/Login.jsx
+++ src/components/authentication/login/Login.jsx
@@ -8,7 +8,7 @@ const Login = () => {
   const [error, setError] = useState("");
   const { login } = useAuth();
   const navigate = useNavigate();
-  alert("git new test :)");
+  alert("git new teasdasst :)");
   const handleLogin = async (e) => {
     //add check whether nickname already exists
     e.preventDefault();
