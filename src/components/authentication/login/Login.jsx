6ebf23c1914a173b002333bab9bd1876f48c6ec3
Kacper Zacharz <xxygrek@wp.pl>
2022-05-05 14:00:40 +0200

    firebase and githyb connection test

--- src/components/authentication/login/Login.jsx
+++ src/components/authentication/login/Login.jsx
@@ -8,6 +8,7 @@ const Login = () => {
   const [error, setError] = useState("");
   const { login } = useAuth();
   const navigate = useNavigate();
+  alert("git test new");
   const handleLogin = async (e) => {
     //add check whether nickname already exists
     e.preventDefault();
