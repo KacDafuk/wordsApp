a584bf18c37c4154f82df1b9e88e367c9acfe42d
Kacper Zacharz <xxygrek@wp.pl>
2022-05-06 09:59:10 +0200

    config update

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -11,13 +11,13 @@ import { getFirestore } from "firebase/firestore";
 //
 console.log(process.env);
 const firebaseConfig = {
-  apiKey: "AIzaSyAD550KcR1NxmFxdjXWpUhu3gPYDJ153w4",
-  authDomain: "learnwords-d7813.firebaseapp.com",
-  projectId: "learnwords-d7813",
-  storageBucket: "learnwords-d7813.appspot.com",
-  messagingSenderId: "34764614129",
-  appId: "1:34764614129:web:a6db7b0066dee0c96f3c50",
-  measurementId: "G-5SG4QQR2VB",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
@@ -25,6 +25,3 @@ const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 export const auth = getAuth(app);
 export const firestore = getFirestore(app);
-
-
-
